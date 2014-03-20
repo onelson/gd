1e90265c06d8c390baabacc363c0c323b15b61a0
briancurtin <brian.curtin@rackspace.com>
2014-03-20 01:02:38 -0500

    Add some get_plate_umpire_tests

--- tests/gd2/test_parser.py
+++ tests/gd2/test_parser.py
@@ -9,7 +9,28 @@ class Test_get_player(unittest.TestCase):
     """Test the gd2.parser.get_player function."""
 
     def test_get_player(self):
-        expected = "test"
+        expected = "Greg Maddux"
         s = stub(attrib=expected)
         actual = parser.get_player(s)
         self.assertEqual(actual, expected)
+
+
+class Test_get_plate_umpire(unittest.TestCase):
+    """Test the gd2.parser.get_plate_umpire function."""
+
+    def test_get_plate_umpire(self):
+        expected = {"position": "home", "name": "Gerry Davis"}
+        element = stub(attrib=expected)
+        tree = stub(findall=lambda arg: [element])
+        actual = parser.get_plate_umpire(tree)
+        self.assertEqual(actual, expected)
+
+    def test_get_plate_umpire_no_pu(self):
+        expected = {"position": "first", "name": "Ted Barrett"}
+        element = stub(attrib=expected)
+        tree = stub(findall=lambda arg: [element])
+        self.assertRaises(parser.ParseError, parser.get_plate_umpire, tree)
+
+    def test_get_plate_umpire_no_umpires(self):
+        tree = stub(findall=lambda arg: [])
+        self.assertRaises(parser.ParseError, parser.get_plate_umpire, tree)
