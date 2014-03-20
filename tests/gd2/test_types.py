e4a81063f2ca2145090a04017f7aa628962db43b
briancurtin <brian.curtin@rackspace.com>
2014-03-20 14:06:55 -0500

    Test that we don't cover anything up when type conversion goes wrong

--- tests/gd2/test_types.py
+++ tests/gd2/test_types.py
@@ -12,3 +12,8 @@ class Test_transform(unittest.TestCase):
         actual_types = {"test1": str, "test2": int, "test3": float}
         types.transform(actual, actual_types)
         self.assertEqual(actual, expected)
+
+    def test_transform_bad_type(self):
+        actual = {"test1": "lol"}
+        actual_types = {"test1": int}
+        self.assertRaises(ValueError, types.transform, actual, actual_types)
