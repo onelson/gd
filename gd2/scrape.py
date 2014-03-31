d269b48d84902d9e87aea2a815a0fa4f32fcb167
briancurtin <brian.curtin@rackspace.com>
2014-03-31 16:27:07 -0500

    get_urls should really be internal, prefix with underscore

--- gd2/scrape.py
+++ gd2/scrape.py
@@ -6,7 +6,7 @@ import requests
 ROOT = "http://gd2.mlb.com/components/game/mlb/"
 
 
-def get_urls(roots, match):
+def _get_urls(roots, match):
     """Given a list of roots, iterate through them and yield links that match.
     If `match` is None, all links are yielded."""
     for root in roots:
@@ -23,34 +23,34 @@ def get_urls(roots, match):
 
 def get_years(root=ROOT):
     """From the root URL, yield URLs to the available years."""
-    yield from get_urls([root], "year")
+    yield from _get_urls([root], "year")
 
 
 def get_months(years):
     """Yield URLs to the available months for every year."""
-    yield from get_urls(years, "month")
+    yield from _get_urls(years, "month")
 
 
 def get_days(months):
     """Yield URLs to the available days for every month."""
-    yield from get_urls(months, "day")
+    yield from _get_urls(months, "day")
 
 
 def get_games(days):
     """Yield URLs to the available games for every day."""
-    yield from get_urls(days, "gid")
+    yield from _get_urls(days, "gid")
 
 
 def get_files(games):
     """Yield URLs to the relevant files for every game."""
     for game in games:
-        yield from get_urls([game], "players.xml")
-        yield from get_urls([game], "game.xml")
-        yield from get_urls([urljoin(game, "inning")], "inning_all.xml")
+        yield from _get_urls([game], "players.xml")
+        yield from _get_urls([game], "game.xml")
+        yield from _get_urls([urljoin(game, "inning")], "inning_all.xml")
 
         # Go another directory deep and get all pitchers and all batters.
-        pitchers = get_urls([game], "pitchers")
-        yield from get_urls(pitchers, None)
+        pitchers = _get_urls([game], "pitchers")
+        yield from _get_urls(pitchers, None)
 
-        batters = get_urls([game], "batters")
-        yield from get_urls(batters, None)
+        batters = _get_urls([game], "batters")
+        yield from _get_urls(batters, None)
